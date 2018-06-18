844ec1570b065f4b0c166e75a41503918c0ac688
Vignesh R <vrs23.92@gmail.com>
2018-06-18 17:25:26 +0530

    Added Search Products by Price functionality. Improved code indentation.

--- library.js
+++ library.js
@@ -1,9 +1,8 @@
+(function(window) {
 
-(function(window){
+    function myLibrary() {
 
-    function myLibrary(){
 
-      
         var catalog = createRandomCatalog(100);
 
 
@@ -14,27 +13,30 @@
             searchAllProducts: searchAllProducts
 
         }
-    
 
-        function searchProductsByType(type){
 
-            var promise = new Promise(function(resolve,reject){
+        function searchProductsByType(type) {
+
+            var promise = new Promise(function(resolve, reject) {
                 var i = 0;
                 var typeArray = [];
-                var possibleTypes = ['Electronics','Book','Clothing','Food'];
-                if(!possibleTypes.includes(type)){
+                var possibleTypes = ['Electronics', 'Book', 'Clothing', 'Food'];
+                if (!possibleTypes.includes(type)) {
                     reject("Invalid Type: " + type)
-                }
-                else{
-                    setTimeout(function(){
-                        while (i < catalog.length){
-                            if (catalog[i].type == type){
-                                typeArray.push({id:catalog[i].id,price:catalog[i].price,type:catalog[i].type});
+                } else {
+                    setTimeout(function() {
+                        while (i < catalog.length) {
+                            if (catalog[i].type == type) {
+                                typeArray.push({
+                                    id: catalog[i].id,
+                                    price: catalog[i].price,
+                                    type: catalog[i].type
+                                });
                             }
                             i++;
                         }
                         resolve(typeArray);
-                    },1000);
+                    }, 1000);
 
                 }
 
@@ -45,25 +47,28 @@
 
 
 
-        function searchProductsByPrice(price,difference){
-            var promise = new Promise(function(resolve,reject){
+        function searchProductsByPrice(price, difference) {
+            var promise = new Promise(function(resolve, reject) {
                 var i = 0;
                 var priceArray = [];
-                if(!isFinite(price)){
+                if (!isFinite(price)) {
                     reject("Invalid Price: " + price)
-                }
-                else{
-                    setTimeout(function(){
-                        while (i < catalog.length){
-                            if (Math.abs(catalog[i].price - price) < difference){
-                                priceArray.push({id:catalog[i].id,price:catalog[i].price,type:catalog[i].type});
+                } else {
+                    setTimeout(function() {
+                        while (i < catalog.length) {
+                            if (Math.abs(catalog[i].price - price) < difference) {
+                                priceArray.push({
+                                    id: catalog[i].id,
+                                    price: catalog[i].price,
+                                    type: catalog[i].type
+                                });
 
                             }
                             i++;
                         }
                         resolve(priceArray);
 
-                    },1000);
+                    }, 1000);
                 }
 
             });
@@ -72,20 +77,24 @@
 
 
 
-        function searchProductById(id){
-        
-            var promise = new Promise(function(resolve,reject){
+        function searchProductById(id) {
+
+            var promise = new Promise(function(resolve, reject) {
                 var i = 0;
-                setTimeout(function(){
-                    while (i < catalog.length){
-                        if (catalog[i].id == id){                        
-                            resolve({id:id,price:catalog[i].price,type:catalog[i].type});
+                setTimeout(function() {
+                    while (i < catalog.length) {
+                        if (catalog[i].id == id) {
+                            resolve({
+                                id: id,
+                                price: catalog[i].price,
+                                type: catalog[i].type
+                            });
                         }
                         i++;
                     }
                     reject("Invalid ID: " + id);
-                
-                },1000);
+
+                }, 1000);
             });
             return promise;
         }
@@ -98,44 +107,49 @@
 
 
 
-
-        function searchAllProducts(){
+        function searchAllProducts() {
             var promise = new Promise(function(resolve, reject) {
 
-                setTimeout(function(){
+                setTimeout(function() {
                     resolve(catalog);
-                },1000);
-            
+                }, 1000);
+
             });
             return promise;
         }
 
 
-        function createRandomProduct(){
-            var typeArray = ['Electronics','Book','Clothing','Food'];
-            var price = (Math.random()*500).toFixed(2) ;
-            var type = typeArray[Math.floor(Math.random()*4)];
+        function createRandomProduct() {
+            var typeArray = ['Electronics', 'Book', 'Clothing', 'Food'];
+            var price = (Math.random() * 500).toFixed(2);
+            var type = typeArray[Math.floor(Math.random() * 4)];
             var valid = Math.random() > .5 ? true : false;
-            return {price:price, type:type};
-            
+            return {
+                price: price,
+                type: type
+            };
+
         }
 
-        function createRandomCatalog(num){
+        function createRandomCatalog(num) {
             var catalog = [];
-            for (var i = 0; i < num; i++){
+            for (var i = 0; i < num; i++) {
                 var obj = createRandomProduct();
-                catalog.push({id:i,price:obj.price,type:obj.type});
+                catalog.push({
+                    id: i,
+                    price: obj.price,
+                    type: obj.type
+                });
             }
             return catalog;
 
         }
-    
+
     }
 
 
-    if(typeof(window.api) === 'undefined'){
+    if (typeof(window.api) === 'undefined') {
         window.api = myLibrary();
     }
 
-})(window); 
-
+})(window);
